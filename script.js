35701ac343f94cd163381c4fd4bbcd192e810342
Your Name <your-email@example.com>
2025-03-26 12:03:23 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(() => console.log('Service Worker registered successfully.'))
+      .catch(error => console.error('Service Worker registration failed:', error));
+  }
+  
